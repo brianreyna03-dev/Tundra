4152fcd24f60e19b3b75102f840a5ea8c3ee0c3c
brianreyna03-dev <brianreyna03@gmail.com>
2026-08-05 11:39:45 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // Replace YOUR-REPOSITORY-NAME with the exact GitHub repository name.
-  base: "github.com/brianreyna03-dev/Tundra/",
+  base: "/Tundra/",
 });
