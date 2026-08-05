68ff34e2a80cc77ecd79fef21f579e5a86738572
brianreyna03-dev <brianreyna03@gmail.com>
2026-08-05 11:29:36 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
-    host: "0.0.0.0",
-    allowedHosts: true,
+    port: 5173,
   },
-
   preview: {
-    host: "0.0.0.0",
-    allowedHosts: true,
+    port: 4173,
   },
 });
