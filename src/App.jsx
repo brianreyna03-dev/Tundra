ac7ef40667ae70a749177941ee3eb7204c3c932f
brianreyna03-dev <brianreyna03@gmail.com>
2026-08-26 05:46:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,7 @@ export default function App() {
             onGenerate={actions.generate}
             onStartManual={actions.startManual}
             onAssign={actions.assignPerson}
+            onSetTraining={actions.setTraining}
             onSetTrainer={actions.setTrainer}
           />
         )}
